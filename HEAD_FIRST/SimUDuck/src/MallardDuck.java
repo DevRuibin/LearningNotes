f6178183a51a9cbfa903fd942008a85692ab3f07
andy <zhangruibin021@gmail.com>
2021-08-23 22:33:43 +0800

    second with interface

--- HEAD_FIRST/SimUDuck/src/MallardDuck.java
+++ HEAD_FIRST/SimUDuck/src/MallardDuck.java
@@ -1,6 +1,16 @@
-public class MallardDuck extends Duck{
+public class MallardDuck extends Duck implements Flyable, Quackable{
     @Override
     protected void display() {
         System.out.println("I am a MallardDuck");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Mallar duck cannot fly");
+    }
+
+    @Override
+    public void quack() {
+        System.out.println("gua gua");
     }
 }
--- HEAD_FIRST/SimUDuck/src/RedheadDuck.java
+++ HEAD_FIRST/SimUDuck/src/RedheadDuck.java
@@ -1,6 +1,16 @@
-public class RedheadDuck extends Duck{
+public class RedheadDuck extends Duck implements Flyable, Quackable{
     @Override
     protected void display() {
         System.out.println("I am Red head duck");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Red head duck fly");
+    }
+
+    @Override
+    public void quack() {
+        System.out.println("gua gua");
     }
 }
